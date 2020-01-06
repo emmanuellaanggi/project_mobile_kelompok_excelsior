b80e9487dfae99e0be24e999ac275e7d8f70d9ae
emmanuella anggi <emma.anggi@gmail.com>
2020-01-06 19:02:39 +0700

    route testing

--- FIRA_APP/lib/_routing/router.dart
+++ FIRA_APP/lib/_routing/router.dart
@@ -10,27 +10,10 @@ import 'package:fira/views/reset_password.dart';
 import 'package:fira/views/user_details.dart';
 import 'package:fira/views/report.dart';
 
+
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
-    case landingViewRoute:
-      return MaterialPageRoute(builder: (context) => LandingPage());
-    case homeViewRoute:
-      return MaterialPageRoute(builder: (context) => HomePage());
-    case loginViewRoute:
-      return MaterialPageRoute(builder: (context) => LoginPage());
-    case registerViewRoute:
-      return MaterialPageRoute(builder: (context) => RegisterPage());
-    case resetPasswordViewRoute:
-      return MaterialPageRoute(builder: (context) => ResetPasswordPage());
-    case chatDetailsViewRoute:
-      return MaterialPageRoute(builder: (context) => ChatDetailsPage(userId: settings.arguments));
-    case userDetailsViewRoute:
-      return MaterialPageRoute(builder: (context) => UserDetailsPage(userId: settings.arguments));
-    case reportViewRoute:
-      return MaterialPageRoute(builder: (context) => ReportPage());
-    case readArticle:
-      return MaterialPageRoute(builder: (context) => ReadArticle());
-      break;
+
     default:
       return MaterialPageRoute(builder: (context) => LandingPage());
   }
