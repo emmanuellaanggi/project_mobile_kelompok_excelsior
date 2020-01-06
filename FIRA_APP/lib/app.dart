2ec26bbf3494f014d0f82ba1e21f87d0570c2de8
emmanuella anggi <emma.anggi@gmail.com>
2020-01-06 17:31:18 +0700

    little fix here and there

--- FIRA_APP/lib/app.dart
+++ FIRA_APP/lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Social',
+      title: 'Fire Rescue Application',
       debugShowCheckedModeBanner: false,
       theme: buildThemeData(),
       onGenerateRoute: router.generateRoute,
