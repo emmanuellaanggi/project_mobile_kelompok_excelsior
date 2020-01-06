1f40b2d3a5573ecbba00f3bb9fbc8d926840d90e
emmanuella anggi <emma.anggi@gmail.com>
2020-01-06 19:03:13 +0700

    make routes in one file to maximize performance

--- FIRA_APP/lib/_routing/router.dart
+++ FIRA_APP/lib/_routing/router.dart
@@ -13,7 +13,25 @@ import 'package:fira/views/report.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
-
+    case landingViewRoute:
+      return MaterialPageRoute(builder: (context) => LandingPage());
+    case homeViewRoute:
+      return MaterialPageRoute(builder: (context) => HomePage());
+    case loginViewRoute:
+      return MaterialPageRoute(builder: (context) => LoginPage());
+    case registerViewRoute:
+      return MaterialPageRoute(builder: (context) => RegisterPage());
+    case resetPasswordViewRoute:
+      return MaterialPageRoute(builder: (context) => ResetPasswordPage());
+    case chatDetailsViewRoute:
+      return MaterialPageRoute(builder: (context) => ChatDetailsPage(userId: settings.arguments));
+    case userDetailsViewRoute:
+      return MaterialPageRoute(builder: (context) => UserDetailsPage(userId: settings.arguments));
+    case reportViewRoute:
+      return MaterialPageRoute(builder: (context) => ReportPage());
+    case readArticle:
+      return MaterialPageRoute(builder: (context) => ReadArticle());
+      break;
     default:
       return MaterialPageRoute(builder: (context) => LandingPage());
   }
